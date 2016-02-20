2ba2f73286222ab61fb000bf1c867253192b3823
Makman2 <makman@alice.de>
2016-02-20 23:37:14 +0100

    WIP

--- modules/AddCoalaTarget.cmake
+++ modules/AddCoalaTarget.cmake
@@ -1,2 +1,5 @@
 # TODO put here "add_coala_target" that generates a target for coala
 #      TODO Also double signature like "execute_coala", but creates a target.
+function(add_coala_target NAME)
+    add_custom_target(${NAME})
+endfunction()
