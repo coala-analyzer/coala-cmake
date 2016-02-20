0578cf1389ede7bbb0febbb81d7fa29b22dc8153
Makman2 <makman@alice.de>
2016-02-20 11:29:28 +0100

    WIP

--- CoalaSupport.cmake
+++ CoalaSupport.cmake
@@ -1,11 +1,3 @@
-# Path to this file.
-# TODO Do I need this one???
-set(COALA_CMAKE_MACRO_DIR ${CMAKE_CURRENT_LIST_DIR})
-
-LIST(APPEND CMAKE_MODULE_PATH "${COALA_CMAKE_MACRO_DIR}")
-
-# Include the cmake files containing the macros and other stuff.
-# include(${COALA_CMAKE_MACRO_DIR}/FindCoala.cmake)
-
-# TODO Create another directory called "macros" or so where I put this stuff
-#      into?
+# Include the cmake files containing the macros and other stuff. Especially
+# needed to enable the `FindCoala` module for `find_package`.
+LIST(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_LIST_DIR}/modules")
---
+++ modules/FindCoala.cmake
@@ -0,0 +1,15 @@
+find_program(
+    COALA_EXECUTABLE
+    NAMES coala
+    DOC "coala - Static code analysis made easy (http://coala-analyzer.org)")
+
+if(COALA_EXECUTABLE)
+    execute_process(COMMAND ${COALA_EXECUTABLE} "--version"
+                    OUTPUT_VARIABLE COALA_VERSION
+                    OUTPUT_STRIP_TRAILING_WHITESPACE)
+endif()
+
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(coala
+                                  REQUIRED_VARS COALA_EXECUTABLE
+                                  VERSION_VAR COALA_VERSION)
